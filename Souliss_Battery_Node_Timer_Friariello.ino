b4a01297c34a48ecb7d3a49a34cdbaa84d85830c
Marcolino <marco.pozzuolo@gmail.com>
2016-01-24 14:44:19 +0100

    nuova versione dello sleep

--- Souliss_Battery_Node_Timer_Friariello.ino
+++ Souliss_Battery_Node_Timer_Friariello.ino
@@ -20,7 +20,7 @@
 ***************************************************************************/
 
 #define	VNET_DEBUG_INSKETCH
-#define VNET_DEBUG  		0
+#define VNET_DEBUG  		1
 
 #define	MaCaco_DEBUG_INSKETCH
 #define MaCaco_DEBUG  		0
@@ -28,53 +28,40 @@
 #define	SLEEPING_INSKETCH
 #define wakePin 		2
 #define	wakePinINT		0
-//#define	wakeupTime	0x0004
-#define	wakeupTime		0x00FF			// The total sleep time is wakeupTime*8 seconds
-#define	wakeupCycles	5		
+#define	wakeupTime		0x01			// The total sleep time is wakeupTime*8 seconds
+#define	wakeupCycles	1		
 
 // Configure the framework
 #include "bconf/DevDuino_v2.h"				//Use DevDuino Version 2 Board
 #include "conf/nRF24L01.h"
 #include "conf/Sleep.h"
-//#include "conf/DynamicAddressing.h"			// Use dynamic addressing
 
 // Include framework code and libraries
 #include <SPI.h>
 #include "Souliss.h"
 
 //Souliss Addressing
-#define network_address         0x6506        // Local address
+#define network_address         0x6502        // Local address
 #define network_my_subnet       0xFF00
 #define network_my_supern       0x6501        // RF24 gateway address
 
 
 //Define the SLOT
 #define BATT_LEVEL		0		//Battery Charge Left
-#define BATT_VOLT		2		//Battery Voltage
-#define TEMPERATURE		4		//Temperature
+
 
 //Define the PIN to keep node asleep during registration
 #define	STAYUP_PIN		3
 #define	StayUp()		digitalRead(STAYUP_PIN)
 
-//Setting Deadband to 0 to all Analog Slot
-#define	Read_AnalogIn(slot)		Souliss_Logic_T51(memory_map, slot, 0, &data_changed)
-#define	Logic_Voltage(slot)		Souliss_Logic_T55(memory_map, slot, 0, &data_changed)
-#define	Logic_Temperature(slot)	Souliss_Logic_T52(memory_map, slot, 0, &data_changed)
-
-//Variables
-float temp=0;
-float vcc_f_dec=0;
-float batterycharge=0;
-
 
 void setup()
 {	
-/*	Serial.begin(115200);
+	Serial.begin(115200);
 	delay(1000);
 	Serial.print("DevDuino INIT-");
 	Serial.println(millis());
-	delay(250);*/
+	delay(250);
 
 	Initialize();
 
@@ -82,9 +69,6 @@ void setup()
 
 	// Set an analog value to measure the battery voltage
 	Set_AnalogIn(BATT_LEVEL);
-	Set_AnalogIn(BATT_VOLT);
-	//Set_Voltage(BATT_VOLT);
-	Set_Temperature(TEMPERATURE);
 
 	WaitSubscription();
 
@@ -120,127 +104,97 @@ void setup()
 }
 
 void loop()
-{   
-
+{
+	// Back to sleep
+	while (isTimeToSleep() && !StayUp())
+	{
+		/**************
+		Add below the code required to sleep custom devices connected, like:
+		- Sensor,
+		- Voltage regulator,
+		- ...
+		**************/
+		Serial.println("Vado a nanna !!");
+		delay(250);
+		// Sleep microcontroller and radio
+		sleepNow();
+	}
 
 	// If the node wake-ups then this statement is executed
-	if(wasSleeping() || StayUp())
-	{	
-		
-		// Here we start to play
-		EXECUTEFAST() {						
-			UPDATEFAST();	
+	if (wasSleeping() || StayUp())
+	{
+		/**************
+		Add below the code required to wakeup custom devices connected, like:
+		- Sensor,
+		- Voltage regulator,
+		- ...
+		**************/
+		Serial.println("Sono sveglio !!");
+		delay(250);
 
-			// Here we process the communication
-			FAST_PeerComms();		
+		// We want send a frame once back from sleep, so we set the trigger and this
+		// will force the node to send its state to the gateway
+		if (!StayUp()) SetTrigger();
 
-			// Check every 510ms how many cycles asleep as expired,
-			// using default values, the nodes stays asleep 2.5 seconds
-			// every 34 minutes
-			FAST_510ms() {
+		// Estimate the battery charge, assuming that you are powering with 2 AA
+		// alkaline
+		//float batterycharge = batteryCharge();
+		float batterycharge = 50 + random(5, 10);
+		Serial.print("Livello Batteria:");
+		Serial.println(batterycharge);
+		delay(250);
 
-				
-				getBatteryInfo();
-				//Serial.print("Send Batt Level:");
-				//Serial.println(batterycharge);
-				//Serial.print("Send Batt voltage:");
-				//Serial.println(vcc_f_dec);
-								
-				ImportAnalog(BATT_LEVEL, &batterycharge);
-				Read_AnalogIn(BATT_LEVEL);
+		ImportAnalog(BATT_LEVEL, &batterycharge);
+		Read_AnalogIn(BATT_LEVEL);
 
-				//vcc_f_dec = 100;
-				ImportAnalog(BATT_VOLT, &vcc_f_dec);
-				//Logic_Voltage(BATT_VOLT);
-				Read_AnalogIn(BATT_VOLT);
-
-				getTemperature();
-				//Serial.print("Send Temperature:");
-				//Serial.println(temp);
-
-				ImportAnalog(TEMPERATURE, &temp);
-				Logic_Temperature(TEMPERATURE);
-			
-				// Back to sleep
-				if(isTimeToSleep() && !StayUp())	
-				{
-					/**************
-					Add below the code required to sleep custom devices connected, like:
-						- Sensor,
-						- Voltage regulator,
-						- ...
-					**************/
-					
-					// Sleep microcontroller and radio
-					//Serial.println("Back to Sleep");
-					//delay(500);
-					sleepNow();
-
-					data_changed=1;
-					
-					/**************
-					Add below the code required to wakeup custom devices connected, like:
-						- Sensor,
-						- Voltage regulator,
-						- ...
-					**************/
-					SetTrigger();
-				}	
-			} 
-			
-		}
-	}	
-} 
-
-void getTemperature(){
-	temp = analogRead(3)*5/1024.0;
-	temp = temp - 0.5;
-	temp = temp / 0.01;
-	//[°C] = ([°F] - 32) × 5/9
-	temp = (temp-32)*0.55;
+		// Here we process the communication
+		ProcessCommunication();
+	}
 }
 
-void getBatteryInfo(){
+float batteryCharge()
+{
 	// Read the input voltage at microcontroller
 	long vcc = readVcc();
-	float vcc_f = (float) vcc;
-				
+	float vcc_f = (float)vcc;
+
 	// Estimate the battery charge, assuming that you are powering with 2 AA
 	// alkaline
-	float mbatt = 3*(vcc_f/1000) - 7.5;
-	batterycharge = 0.66 + 0.022*mbatt + (float)(0.0074*pow(mbatt,3)) + (float)(0.0088*pow(mbatt,9));
-				
+	float mbatt = 3 * (vcc_f / 1000) - 7.5;
+	float batterycharge = 0.66 + 0.022*mbatt + (float)(0.0074*pow(mbatt, 3)) + (float)(0.0088*pow(mbatt, 9));
+
 	// Cut if out of range
-	if(batterycharge > 1) batterycharge = 1;
-	if(batterycharge < 0) batterycharge = 0;
-				
+	if (batterycharge > 1) batterycharge = 1;
+	if (batterycharge < 0) batterycharge = 0;
+
 	// Get it in percentage
-	batterycharge*=100;
-	vcc_f_dec=vcc_f/1000;
+	batterycharge *= 100;
+
+	return batterycharge;
 }
 
 long readVcc() {
-	
+
 	// Read 1.1V reference against AVcc
 	// set the reference to Vcc and the measurement to the internal 1.1V reference
-	#if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-		ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-	#elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
-		ADMUX = _BV(MUX5) | _BV(MUX0);
-	#elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
-		ADMUX = _BV(MUX3) | _BV(MUX2);
-	#else
-		ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-	#endif  
+#if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+	ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+#elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+	ADMUX = _BV(MUX5) | _BV(MUX0);
+#elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+	ADMUX = _BV(MUX3) | _BV(MUX2);
+#else
+	ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+#endif  
 
 	delay(75); // Wait for Vref to settle
 	ADCSRA |= _BV(ADSC); // Start conversion
-	while (bit_is_set(ADCSRA,ADSC)); // measuring
+	while (bit_is_set(ADCSRA, ADSC)); // measuring
 
-	uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH  
+	uint8_t low = ADCL; // must read ADCL first - it then locks ADCH  
 	uint8_t high = ADCH; // unlocks both
 
-	long result = (high<<8) | low;
+	long result = (high << 8) | low;
 
 	result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
 	return result; // Vcc in millivolts
